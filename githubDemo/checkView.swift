cf15e40cd5cc2ae0b8cd0d400e41c6b3e7a29f8f
saloni <saloni.chandwani@differenzsystem.com>
2024-01-24 16:44:54 +0530

    to add whole file

---
+++ githubDemo/checkView.swift
@@ -0,0 +1,18 @@
+//
+//  checkView.swift
+//  githubDemo
+//
+//  Created by differenz47 on 24/01/24.
+//
+
+import SwiftUI
+
+struct checkView: View {
+    var body: some View {
+        Text("Hello, World! to check Gitpush")
+    }
+}
+
+#Preview {
+    checkView()
+}
