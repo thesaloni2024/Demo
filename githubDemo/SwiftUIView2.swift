a87a9b7ad8043a484e56252cdb95a5e9630e1d30
saloni <saloni.chandwani@differenzsystem.com>
2024-01-24 16:47:56 +0530

    to add whole file

---
+++ githubDemo/SwiftUIView2.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView2.swift
+//  githubDemo
+//
+//  Created by differenz47 on 24/01/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    var body: some View {
+        Text("Hello, World! swift 222")
+    }
+}
+
+#Preview {
+    SwiftUIView2()
+}
---
+++ githubDemo/tocheckView.swift
@@ -0,0 +1,18 @@
+//
+//  tocheckView.swift
+//  githubDemo
+//
+//  Created by differenz47 on 24/01/24.
+//
+
+import SwiftUI
+
+struct tocheckView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tocheckView()
+}
