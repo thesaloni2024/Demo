b872009b9c2867994b4a9388beaade36063cd35b
saloni <saloni.chandwani@differenzsystem.com>
2024-01-24 16:30:29 +0530

    Initial Commit

---
+++ githubDemo/githubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubDemoApp.swift
+//  githubDemo
+//
+//  Created by differenz47 on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct githubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
